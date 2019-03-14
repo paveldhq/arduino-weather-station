ab145e356badf9887b9b1c68efdbbc5f1a24f7bb
Pavlo Ratushnyi <pratushnyi@smartling.com>
2019-03-14 23:12:05 +0200

    fixed display lib

--- indoor-sensor/indoor-sensor.ino
+++ indoor-sensor/indoor-sensor.ino
@@ -1,23 +1,28 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
-#include <Wire.h> 
-#include "libs/LiquidCrystal_I2C/LiquidCrystal_I2C.h"
+/**
+ * indoor sensor firmware based on ATMEGA328p (arduino nano / pro )
+ * - DEBUG: 1602 LCD screen (i2c)
+ * - Temperature / Humidity / Preassure: BME 280 (i2C)
+ * - CO2: MH-Z19B
+ * - Dust Sensor Module DSM501A PM2.5
+ * - CO / NH3 / NO2 - MICS-6814
+ * - IO: HC12 (HC11)
+ */
+ 
+#include "libs/lcd/LiquidCrystal_I2C.h"
+#define DISPLAY_I2C_ADDRESS 0x3F
+#define DISPLAY_CHARS 16
+#define DISPLAY_LINES 2
 
-LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(DISPLAY_I2C_ADDRESS, DISPLAY_CHARS, DISPLAY_LINES); 
 
 void setup()
 {
-  lcd.init();                      // initialize the lcd 
-  // Print a message to the LCD.
+  lcd.init();
   lcd.backlight();
-   lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Arduino LCM I");
-   lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Power By Ec-yuan!");
 }
 
-
-void loop()
-{
-}
+void loop(){}
